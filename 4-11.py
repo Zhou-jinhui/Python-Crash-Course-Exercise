464f1048f70b73b77dc8ad33bcc0e68cac8da0d8
Zhou-jinhui <46165895+Zhou-jinhui@users.noreply.github.com>
2018-12-27 15:14:08 +0800

    Add files via upload

---
+++ 4-11.py
@@ -0,0 +1,18 @@
+magicians = ["alice", "david", "carolina"]
+for magician in magicians:
+	print(magician.title() + ", that was a great trick!")
+	print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+pizzas = ["apple", "tomato", "banana"]
+for pizza in pizzas:
+	print("I like " + pizza + " pizza.\n")
+print("I really like Pizza.")
+
+
+animals = ["dog", "cat", "tiger"]
+for animal in animals:
+	print(animal)
+	print("A " + animal + " has four legs.")
+print("\nAll these animals has four legs.")
